b48078d8b0b3ae1927b75f827d51b5defd73e286
ociobenablement/identitycloudservice/chiping.hwang@oracle.com <test@example.com>
2022-06-16 09:31:53 -0700

    v2

--- orm/varaiables.tf
+++ orm/varaiables.tf
@@ -27,6 +27,77 @@ variable "release" {
 
 
 
+# variables for dynamic group
+# variable for ocid of compartment that holds OKE cluster
+variable "node_compartment_ocid" {
+    type = string
+    default = ""
+}
+variable "dynamic_group_name" {
+    type = string
+    default = "mesh_osok"
+}
+variable "dynamic_group_description" {
+    type = string
+    default = "osok dynamic group"
+}
+
+
+# variables for Polciy for Dynamic Group to allow OSOK Operations
+# variable for ocid of compartment that holds OKE cluster
+
+variable "policy_description" {
+    type = string
+    default = "policy to allow OSOK operation"
+}
+
+variable "policy_name" {
+    type = string
+    default = "osok_policy"
+}
+
+
+
+# variables for Kube.tf
+
+
+variable "kube_config_path" {
+    type = string
+    default = "/tmp/kubeconfig"
+}
+
+# context within .kubeconfig to use
+variable "config_context" {
+    type = string
+    default = ""
+
+}
+
+
+
+
+
+
+
+/*
+variable "registry_display_name" {
+  default = "node-express-getting-starter"
+}
+*/
+
+
+
+/*
+variable "deploy_artifact_display_name" {
+  default = "node-express-getting-starter"
+}
+*/
+
+
+#variable "ocir_region" {
+# default = "iad"}
+
+
 
 
 
